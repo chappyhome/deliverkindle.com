043e9be112e08d0aad2a1bcc0774f075f0297d57
pengwei <sgqjjpw@gmail.com>
2013-09-13 20:57:00 +0800

    change cover path ,or not sendfile

--- routes/getData.js
+++ routes/getData.js
@@ -48,16 +48,18 @@ exports.getCoverPath = function(req, res){
 	redisClient.hget(CALIBRE_ALL_BOOKS_HASH, id, function(err, reply){
 		var r = JSON.parse(reply);
 
-		path = (r != null)?p.join(root,'cover/' + r.path + '/cover_128_190.jpg'):p.join(root,'assets/images/cover_128_190.jpg');
+		//path = (r != null)?p.join(root,'cover/' + r.path + '/cover_128_190.jpg'):p.join(root,'assets/images/cover_128_190.jpg');
+		path = (r != null)?'cover/' + r.path + '/cover_128_190.jpg':'assets/images/cover_128_190.jpg';
 		console.log(path);
-		fs.exists(path, function(exists) {
-			if(exists){
-				res.sendfile(path);
-			}else{
-				res.sendfile(p.join(root,'assets/images/cover_128_190.jpg'));
-			}
+		res.send(path);
+		// fs.exists(path, function(exists) {
+		// 	if(exists){
+		// 		res.sendfile(path);
+		// 	}else{
+		// 		res.sendfile(p.join(root,'assets/images/cover_128_190.jpg'));
+		// 	}
 				
-		});
+		// });
 	});
 
 
