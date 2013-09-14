cc33ab79215c589b3ded221ea8dec1d90618f28c
pengwei <sgqjjpw@gmail.com>
2013-09-14 19:26:49 +0800

    add id and row into localStorage

--- routes/getData.js
+++ routes/getData.js
@@ -117,14 +117,23 @@ exports.getRedisRankBooks = function(req, res) {
 exports.startReader = function(req, res) {
 	var bookid = req.params.id;
 	if(!bookid) return res.send(404);
-	redisClient.hget(CALIBRE_EPUB_PATH_HASH, bookid, function(err, path){
-		var real_epub_path = watchPath + "/" + path;
+	redisClient.hget(CALIBRE_ALL_BOOKS_HASH, bookid, function(err, row){
+		var real_epub_path = watchPath + "/" + row.path;
 		console.log(real_epub_path);
 		fs.exists(real_epub_path, function(exists) {
 			if(exists){
-				var unzip_dir = "epub_content/" + path + "/";//CALIBRE_ALL_BOOKS_CLICK_HASH
-				redisClient.hincrby(CALIBRE_ALL_BOOKS_CLICK_HASH, bookid, 1);
-				res.render('index', { epub_path: unzip_dir });
+				var unzip_dir = "epub_content/" +row.path + "/";//CALIBRE_ALL_BOOKS_CLICK_HASH
+				//redisClient.hincrby(CALIBRE_ALL_BOOKS_CLICK_HASH, bookid, 1);
+				var hash_key = "CalibreBookDetailDataHash";
+				var list_key = "CalibreBookIdList";
+				var dict = {
+					epub_path: unzip_dir,
+					books_data_hash: hash_key,
+					row: row,
+					bookid: bookid,
+					books_id_list: list_key
+				};
+				res.render('index', dict);
 			}else{
 				res.send(404);
 			}
